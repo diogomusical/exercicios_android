d78de584bc4e0645c23b575dcb9af7063b38cab0
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-03-20 10:40:35 +0000

    06_Exercicio3_Criação_da_MainActivity
    Criei a MainActivity e coloquei os botões a reencaminhar para as Activities correspondente (Registo, Login, Sobre)

--- 06_Exercicio3_Aplicacao_de_Registo_Login_Sobre/app/src/main/java/com/example/a06_exercicio3_aplicacao_de_registo_login_sobre/MainActivity.kt
+++ 06_Exercicio3_Aplicacao_de_Registo_Login_Sobre/app/src/main/java/com/example/a06_exercicio3_aplicacao_de_registo_login_sobre/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a06_exercicio3_aplicacao_de_registo_login_sobre
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a06_exercicio3_aplicacao_de_registo_login_sobre.databinding.ActivityMainBinding
@@ -12,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonRegisto.setOnClickListener {
+            val i: Intent = Intent(this, RegistoActivity::class.java)
+            startActivity(i)
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            val i: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
+
+        binding.buttonSobre.setOnClickListener {
+            val i: Intent = Intent(this, SobreActivity::class.java)
+            startActivity(i)
+        }
     }
 }
