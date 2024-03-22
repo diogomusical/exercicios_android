9106554d3ccb83ff17654207c52e4674afcfade2
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-03-22 22:57:28 +0000

    06_Exercicio4_MainActivity_completa

--- 06_Exercicio4_Pedido_Restaurante/app/src/main/java/com/example/a06_exercicio4_pedido_restaurante/MainActivity.kt
+++ 06_Exercicio4_Pedido_Restaurante/app/src/main/java/com/example/a06_exercicio4_pedido_restaurante/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a06_exercicio4_pedido_restaurante
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a06_exercicio4_pedido_restaurante.databinding.ActivityMainBinding
@@ -12,5 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonPedido.setOnClickListener {
+
+            val entradas = binding.checkboxEntradas.isActivated
+            val prato_carne = binding.checkboxPratoCarne.isActivated
+            val prato_peixe = binding.checkboxPratoPeixe.isActivated
+            val prato_vegetariano = binding.checkboxPratoVegetariano.isActivated
+            val vinho = binding.checkboxVinho.isActivated
+            val refrigerante = binding.checkboxRefrigerante.isActivated
+            val agua = binding.checkboxAgua.isActivated
+            val sobremesa = binding.checkboxSobremesa.isActivated
+            val cafe = binding.checkboxCafe.isActivated
+
+            val i: Intent = Intent(this, SplashScreenPedidoActivity::class.java)
+            i.putExtra("entradas",entradas)
+            i.putExtra("prato_carne", prato_carne)
+            i.putExtra("prato_vegetariano", prato_vegetariano)
+            i.putExtra("vinho", vinho)
+            i.putExtra("refrigerante", refrigerante)
+            i.putExtra("agua", agua)
+            i.putExtra("sobremesa", sobremesa)
+            i.putExtra("cafe", cafe)
+            startActivity(i)
+        }
     }
 }
